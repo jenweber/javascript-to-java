4cdd1fe683a37a873a931a7f2693a184254c9058
JenWeber <weberj10@gmail.com>
2016-05-23 14:26:16 -0400

    draft arrays content page

--- app/router.js
+++ app/router.js
@@ -8,6 +8,7 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('comparing-languages');
   this.route('java-overview');
+  this.route('arrays');
 });
 
 export default Router;
